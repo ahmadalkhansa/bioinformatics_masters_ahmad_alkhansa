c06381823f2f9c134fee6c2ee179f50b5b6514e7
ahmadalkhansa <ahmadkhansa95@gmail.com>
2018-12-11 15:42:14 +0100

    new

---
+++ python_exercises/pyth001.py
@@ -0,0 +1,43 @@
+#1
+x = 7
+print(x)
+#2
+y = 3.14
+#3
+num = int(input("put number here: ")
+if num % 2 == 0:
+   print("even")
+else:
+   print("odd")
+#4
+average = (x + y)/2
+print average
+#5
+s1 = abs(x - average)
+s2 = abs(y - average)
+print(s1)
+print(s2)
+#6
+average_s = (s1 + s2)/2
+print(average_s)
+#7
+z = 2 * x - 1
+print(z)
+#8
+sorry :(
+#9
+x1 = 23
+x2 = 9
+y1 = 84
+y2 = 66
+#10
+from math import sqrt
+euclidean_distance = sqrt((y1 - x1)**2 + (y2 - x2)**2)
+print(euclidean_distance)
+#11
+total = x1 + x2 + y1 + y2
+def p(variable):
+    p = variable / total
+    return p
+print(p(x1))
+
