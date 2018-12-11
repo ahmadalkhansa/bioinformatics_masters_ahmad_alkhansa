b49673e8bbfe8ec25740bde16751325fedaed8b1
ahmadalkhansa <45216804+ahmadalkhansa@users.noreply.github.com>
2018-12-11 13:25:24 +0100

    Add files via upload

---
+++ exam/average_function.py
@@ -0,0 +1,17 @@
+# average_function.py
+# For this exercise the pseudo-code is required (in this same file) 
+# Write a function that calculates the average of the values of
+# any vector of 10 numbers 
+# Each single value of the vector should be read from the keyboard
+# and added to a list.
+# Print the input vector and its average 
+# Define separate functions for the input and for calculating the average
+
+'''
+pseudo-code:
+'''
+1. define a functione called averagefunction of arguments vector
+2. for each element inside the vector add to the previous one and continue till the end of the list
+3. divide the sum over 2
+4. return the value
+5. print the value
